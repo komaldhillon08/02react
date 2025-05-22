2f43484bf79b873d8fda13c736e1f106ef3feb51
komaldhillon08 <komaldhillonpb19@gmail.com>
2025-05-22 23:27:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  let [counter , setCounter] = useState(0)
+
+  // let counter = 5 ;
+  let addValue = () => {
+
+    // counter =  counter + 1 ;
+
+    // this is one way 
+    // setCounter(counter + 1) 
+
+  // this condition is not increment up to 20 
+     if (counter >= 20) {
+      console.log("counter is 20 that is why stope");
+      return ;
+    }
+     // this is second way 
+    counter = counter +1 
+    setCounter(counter)
+    console.log("counter", counter , Math.random()  );
+  }
+
+  // remove 
+  let remove = () => {
+
+  // this condition is not decrement in 0 
+      if (counter <= 0) {
+    console.log("counter is 0 that is why not decrement " );
+    return
+  }
+      counter = counter - 1 
+      setCounter(counter)
+
+  }
+
+  return (
+    <>
+     <h1>komalpreet singh</h1>
+     <h2>Counter Value {counter}</h2>
+
+    <button onClick={addValue}>Add Value {counter}</button> <br />
+    <button onClick={remove}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
